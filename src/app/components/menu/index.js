bf8bbb2729ace32d1b42a1f826bbccdb761be9b0
Anil <ak26122k@gmail.com>
2023-10-29 21:30:58 +0530

    start from scratach

---
+++ src/app/components/menu/index.js
@@ -0,0 +1,31 @@
+
+import {BiSolidPencil,BiSolidEraser,BiSolidDownload,BiLeftArrowCircle,BiRightArrowCircle,BiRevision} from "react-icons/bi"
+
+
+export default function Menu(){
+    return (
+      <div>
+        <div>
+       <BiSolidPencil/>
+        </div>
+        <div>
+       <BiSolidEraser/>
+        </div>
+        <div>
+       <BiLeftArrowCircle/>
+        </div>
+        
+        <div>
+       <BiRightArrowCircle/>
+        </div>
+        <div>
+       <BiRevision/>
+        </div>
+        
+        <div>
+       <BiSolidDownload/>
+        </div>
+       
+      </div>
+    )
+}
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+
+
+import Menu from "./components/menu"
+export default function Home() {
+  return (
+    <main>
+        <Menu/>
+     
+    </main>
+  )
+}
