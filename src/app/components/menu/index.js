e408ac877fef244847d052b6e380bc2f81f1924f
Anil <ak26122k@gmail.com>
2023-10-30 13:46:22 +0530

    memu complete

--- src/app/components/menu/index.js
+++ src/app/components/menu/index.js
@@ -4,26 +4,26 @@ import {BiSolidPencil,BiSolidEraser,BiSolidDownload,BiLeftArrowCircle,BiRightArr
 
 export default function Menu(){
     return (
-      <div>
-        <div>
-       <BiSolidPencil/>
+      <div className="absolute flex left-1/2 shadow-md bg- transform -translate-x-1/2 w-1/4  justify-around top-10  py-2 rounded-md border-2 border-gray-300">
+        <div className="cursor-pointer h-9 w-10 flex justify-center items-center rounded-md">
+       <BiSolidPencil className="text-2xl"/>
         </div>
-        <div>
-       <BiSolidEraser/>
+        <div className="cursor-pointer h-9 w-10 flex justify-center items-center rounded-md">
+       <BiSolidEraser className="text-2xl"/>
         </div>
-        <div>
-       <BiLeftArrowCircle/>
-        </div>
-        
-        <div>
-       <BiRightArrowCircle/>
-        </div>
-        <div>
-       <BiRevision/>
+        <div className="cursor-pointer h-9 w-10 flex justify-center items-center rounded-md">
+       <BiLeftArrowCircle className="text-2xl"/>
         </div>
         
-        <div>
-       <BiSolidDownload/>
+        <div className="cursor-pointer h-9 w-10 flex justify-center items-center rounded-md">
+       <BiRightArrowCircle className="text-2xl"/>
+        </div>
+        <div className="cursor-pointer h-9 w-10 hover:bg-red-600 flex justify-center items-center rounded-md">
+       <BiRevision className="text-2xl"/>
+        </div>
+        
+        <div className="cursor-pointer h-9 w-10 flex justify-center items-center rounded-md">
+       <BiSolidDownload className="text-2xl"/>
         </div>
        
       </div>
